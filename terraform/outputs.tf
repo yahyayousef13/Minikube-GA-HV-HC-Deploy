1cfa02eca6aa51797900cfc1c8b32bec48e24841
Ashishm96 <ashishmakasare24@gmail.com>
2025-03-07 23:05:38 +0530

    updated terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,17 +2,9 @@ output "minikube_status" {
   value = fileexists("minikube_status.txt") ? file("minikube_status.txt") : "Minikube status unknown"
 }
 
-output "helm_app_status" {
-  description = "Helm deployment status"
-  value       = helm_release.my_app.status
-}
+
 
 output "github_runner_status" {
   description = "GitHub Runner Status"
   value       = null_resource.github_runner.id != "" ? "GitHub Runner is configured and running!" : "GitHub Runner setup failed."
-}
-
-output "monitoring_status" {
-  description = "Monitoring Stack Deployment"
-  value       = helm_release.monitoring.status
 }
